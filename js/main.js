38a318a25460b219ef530890b59c33e402881177
iamkirabond <kors9442@gmail.com>
2019-12-09 00:27:34 +0300

    added js code, ymaps unfinished

--- js/main.js
+++ js/main.js
@@ -12,17 +12,26 @@ $('.side-menu__btn').on('click', function(){
 
 
 /* ORDER CALL   */
-$('.offer__btn, .help__btn, .call-us__btn').on('click',function(){
+$('.offer__btn, .help__btn, .call-us__btn, .call').on('click',function(){
+    event.preventDefault();
     $('.order-call').toggle();
+    $('html, body').css({
+        overflow: 'hidden',
+    });
 })
-$('.order-call__close-btn').on('click',function(){
+$('.order-call__close-btn').on('click',function(event){
+    event.preventDefault();
     $('.order-call').hide();
+    $('html, body').css({
+        overflow: 'visible'
+    });
 })
 /* ORDER CALL  END */
 
 
 /* VALIDATION  */
-$("#order-form").validate({
+
+$('#order-form').validate({
     rules: {
         firstName: {
             requiered: true
@@ -59,9 +68,45 @@ $("#order-form").validate({
 /*FIXED HEADER  END*/
 
 /*  PARALLAX  */
-var scene = $('#scene').get(0);
 
-console.log(scene)
+var scene = $('#scene').get(0);
 var parallaxInstance = new Parallax(scene);
-console.log(parallaxInstance)
+
 /*  PARALLAX  END*/
+
+
+
+/* SLIDER  */
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next-unique',
+        prevEl: '.swiper-button-prev-unique'
+    },
+    breakpoints: {
+        998: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 3
+        }
+    }
+  });
+/* SLIDER END */
+
+
+/* Yandex Map */
+
+ymaps.ready(init);
+function init(){
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 12
+    });
+}
+/* Yandex Map  END*/
